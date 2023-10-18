815b0827e588e3bf2d734bc86caa35497da19095
Jose Lima <jalimag0206@gmail.com>
2023-10-18 18:15:22 -0300

    feat: ensure user goes to catalog page on catalog button click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -10,11 +10,12 @@ import {
 } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarImage} from "./avatar"
 import { AvatarFallback } from "@radix-ui/react-avatar";
 import { Separator } from "./separator";
+import Link from "next/link";
 
 const Header = () => {
   const { status, data } = useSession()
@@ -85,20 +86,28 @@ const Header = () => {
               </Button>
             )}
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <HomeIcon size={16}/>
-              Inicio
-            </Button>
+            <SheetClose asChild>
+              <Link href="/">
+                <Button variant="outline" className="w-full justify-start gap-2">
+                  <HomeIcon size={16}/>
+                  Inicio
+                </Button>
+              </Link>
+            </SheetClose>
 
             <Button variant="outline" className="w-full justify-start gap-2">
               <PercentIcon size={16}/>
               Ofertas
             </Button>
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <ListOrderedIcon size={16}/>
-              Catálogo
-            </Button>
+            <SheetClose asChild>
+              <Link href='/catalog'>
+                <Button variant="outline" className="w-full justify-start gap-2">
+                  <ListOrderedIcon size={16}/>
+                  Catálogo
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
