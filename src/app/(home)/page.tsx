16ae10d00cd731587ae4d2d5d906c6c7c7d66516
Jose Lima <jalimag0206@gmail.com>
2023-10-18 04:15:37 -0300

    feat: add keyboards and mouse sections

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import Categories from './components/categories'
 import { prismaClient } from '@/lib/prisma'
 import ProductList from './components/product-list';
+import SectionTitle from './components/section-title';
+import PromoBanner from './components/promo-banner';
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -12,34 +14,59 @@ export default async function Home() {
     },
   });
 
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
+
   return (
-    <div className=''>
-      <Image 
+    <div className='flex flex-col gap-8'>
+      <PromoBanner 
         src="/banner-home-01.png"
-        width={0}
-        height={0} 
-        className='h-auto w-full px-5'
-        sizes='100vw'
         alt="Até 55% de desconto"      
       />
 
-      <div className='mt-8 px-5'>
+      <div className='px-5'>
         <Categories />
       </div>
 
-      <div className='mt-8'>
-        <p className="pl-5 mb-3 font-bold uppercase">Ofertas</p>
+      <div>
+        <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals}/>
       </div>
 
-      <Image 
+      <PromoBanner 
         src="/banner-home-02.png"
-        width={0}
-        height={0} 
-        className='h-auto w-full px-5'
-        sizes='100vw'
         alt="Até 55% de desconto em mouses!"      
       />
+
+      <div>
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards}/>
+      </div>
+
+      <div>
+        <PromoBanner
+          src="/banner-home-03.png"
+          alt="Até 20% de desconto em fones!"
+        />
+      </div>
+
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses}/>
+      </div>
     </div>
   )
 }
