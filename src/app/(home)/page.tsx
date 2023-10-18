0e24e717e89493affcc624cea7aa3a5042347267
Jose Lima <jalimag0206@gmail.com>
2023-10-18 03:56:28 -0300

    feat: add second banner homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -28,8 +28,18 @@ export default async function Home() {
       </div>
 
       <div className='mt-8'>
+        <p className="pl-5 mb-3 font-bold uppercase">Ofertas</p>
         <ProductList products={deals}/>
       </div>
+
+      <Image 
+        src="/banner-home-02.png"
+        width={0}
+        height={0} 
+        className='h-auto w-full px-5'
+        sizes='100vw'
+        alt="Até 55% de desconto em mouses!"      
+      />
     </div>
   )
 }
