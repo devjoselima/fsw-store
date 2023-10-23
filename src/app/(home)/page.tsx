f7ea8f76524f6e2b212a2436c9f3383c039452aa
Jose Lima <jalimag0206@gmail.com>
2023-10-23 00:39:56 -0300

    feat: add recommended products title to product details page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "../../components/ui/product-list";
-import SectionTitle from "./components/section-title";
+
 import PromoBanner from "./components/promo-banner";
+import SectionTitle from "@/components/ui/section-title";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
