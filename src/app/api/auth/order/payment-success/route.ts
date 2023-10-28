f73d01fca6b08a933ea2736933797c7224aacbe1
Jose Lima <jalimag0206@gmail.com>
2023-10-28 15:57:50 -0300

    feat: ensure order is updated on checkout success

--- src/app/api/auth/order/payment-success/route.ts
+++ src/app/api/auth/order/payment-success/route.ts
@@ -1,11 +1,13 @@
+import { prismaClient } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
 });
+
 export const POST = async (request: Request) => {
-  const signature = request.headers.get("stripe-signature")!;
+  const signature = request.headers.get("stripe-signature");
 
   if (!signature) {
     return NextResponse.error();
@@ -28,8 +30,17 @@ export const POST = async (request: Request) => {
         expand: ["line_items"],
       },
     );
-
     const lineItems = sessionWithLineItems.line_items;
+
+    // ATUALIZAR PEDIDO
+    await prismaClient.order.update({
+      where: {
+        id: session.metadata.orderId,
+      },
+      data: {
+        status: "PAYMENT_CONFIRMED",
+      },
+    });
   }
 
   return NextResponse.json({ received: true });
