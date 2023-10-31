c8b2330a112e8bd5b36170047353a4f744c9af7f
Jose Lima <jalimag0206@gmail.com>
2023-10-31 20:14:46 -0300

    feat: add first desktop banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "../../components/ui/product-list";
+import Image from "next/image";
 
 import PromoBanner from "./components/promo-banner";
 import SectionTitle from "@/components/ui/section-title";
@@ -31,39 +32,55 @@ export default async function Home() {
   });
 
   return (
-    <div className="flex flex-col gap-8 py-8">
-      <PromoBanner src="/banner-home-01.png" alt="Até 55% de desconto" />
-
-      <div className="px-5">
-        <Categories />
-      </div>
-
-      <div>
-        <SectionTitle>Ofertas</SectionTitle>
-        <ProductList products={deals} />
-      </div>
-
-      <PromoBanner
-        src="/banner-home-02.png"
-        alt="Até 55% de desconto em mouses!"
-      />
-
-      <div>
-        <SectionTitle>Teclados</SectionTitle>
-        <ProductList products={keyboards} />
-      </div>
-
-      <div>
-        <PromoBanner
-          src="/banner-home-03.png"
-          alt="Até 20% de desconto em fones!"
+    <>
+      <div className="mx-auto max-w-[1920px]">
+        <Image
+          src="/banner-home-desktop.png"
+          className="hidden h-auto w-full lg:block"
+          width={0}
+          height={0}
+          sizes="100vw"
+          alt="Até 55% de desconto esse mês!"
         />
       </div>
+      <div className="container flex flex-col gap-8 py-8">
+        <PromoBanner
+          src="/banner-home-01.png"
+          alt="Até 55% de desconto"
+          className="px-5 lg:hidden"
+        />
 
-      <div>
-        <SectionTitle>Mouses</SectionTitle>
-        <ProductList products={mouses} />
+        <div className="px-5">
+          <Categories />
+        </div>
+
+        <div>
+          <SectionTitle>Ofertas</SectionTitle>
+          <ProductList products={deals} />
+        </div>
+
+        <PromoBanner
+          src="/banner-home-02.png"
+          alt="Até 55% de desconto em mouses!"
+        />
+
+        <div>
+          <SectionTitle>Teclados</SectionTitle>
+          <ProductList products={keyboards} />
+        </div>
+
+        <div>
+          <PromoBanner
+            src="/banner-home-03.png"
+            alt="Até 20% de desconto em fones!"
+          />
+        </div>
+
+        <div>
+          <SectionTitle>Mouses</SectionTitle>
+          <ProductList products={mouses} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={inter.className}>
         <div className="flex h-full flex-col">
           <AuthProvider>
